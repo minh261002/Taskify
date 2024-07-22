bd8ce5804d7a3e278f66bef9d40d23d3060b5d6e
minhttran <trcongminh.261002@gmail.com>
2024-07-22 13:45:56 +0700

    Organization config

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,12 @@
+import { auth } from '@clerk/nextjs/server';
+
+const OrganizationIdPage = () => {
+
+    return (
+        <div>
+            Organization Page
+        </div>
+    )
+}
+
+export default OrganizationIdPage
